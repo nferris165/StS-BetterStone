4011cee30a43340db2baec4b0e8343b5aa7236cd
nferris165 <51347535+nferris165@users.noreply.github.com>
2020-07-30 03:01:23 -0400

    updated metrics
    added mod list to metric data

--- src/main/java/betterStone/patches/customMetrics.java
+++ src/main/java/betterStone/patches/customMetrics.java
@@ -6,6 +6,7 @@ import betterStone.events.BetterStoneEvent;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Net;
 import com.badlogic.gdx.net.HttpRequestBuilder;
+import com.evacipated.cardcrawl.modthespire.Loader;
 import com.google.gson.Gson;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -13,6 +14,7 @@ import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.rooms.VictoryRoom;
 import com.megacrit.cardcrawl.screens.DeathScreen;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
@@ -135,6 +137,24 @@ public class customMetrics implements Runnable {
         addData("act_limit", BetterStone.actLimit);
         addData("ascension_limit", !BetterStone.disableAscLimit);
         addData("actNum", AbstractDungeon.actNum);
+        addData("mods", getModList());
+    }
+
+    private String getModList(){
+        StringBuilder retVal = new StringBuilder();
+        String mod;
+        for(int i = 0; i < Loader.MODINFOS.length; ++i) {
+            if(i != 0){
+                retVal.append("-|-");
+            }
+            mod = StringUtils.substring(Loader.MODINFOS[i].Name, 0, 30);
+            retVal.append(mod);
+            if(retVal.length() >= 1950){
+                retVal.append("---MAX SIZE REACHED!!!");
+                break;
+            }
+        }
+        return retVal.toString();
     }
 
     private HashMap getDeck(){
